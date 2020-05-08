a60444759f6db2ddbd1d71120a0f281817a0634b
idong00 <idong00@naver.com>
2020-05-08 21:14:27 +0900

    Overlapped IO Model 1

--- IO/overlapped/overlappedServer/overlappedServer.cpp
+++ IO/overlapped/overlappedServer/overlappedServer.cpp
@@ -20,10 +20,11 @@
 #define PORT 5150
 #define DATA_BUFSIZE 8192
 
+// 소켓 정보 구조체
 typedef struct _SOCKET_INFORMATION {
-    CHAR Buffer[DATA_BUFSIZE];
-    WSABUF DataBuf;
-    SOCKET Socket;
+    CHAR Buffer[DATA_BUFSIZE];	// 버퍼
+    WSABUF DataBuf;		// 실제버퍼 (현재 구조체가 가리키는 버퍼)
+    SOCKET Socket;		// 소켓핸들
     WSAOVERLAPPED Overlapped;
     DWORD BytesSEND;
     DWORD BytesRECV;
@@ -47,6 +48,7 @@ int main(int argc, char **argv)
 
     InitializeCriticalSection(&CriticalSection);
 
+	// 윈속 dll 로딩
     if (WSAStartup((2, 2), &wsaData) != 0)
     {
         printf("WSAStartup() failed with error %d\n", WSAGetLastError());
@@ -56,6 +58,12 @@ int main(int argc, char **argv)
     else
         printf("WSAStartup() looks nice!\n");
 
+	// ---------------------------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------------------------
+
+
+	//
+	// 리스닝 소켓 만듬
     if ((ListenSocket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED)) == INVALID_SOCKET)
     {
         printf("Failed to get a socket %d\n", WSAGetLastError());
@@ -68,6 +76,11 @@ int main(int argc, char **argv)
     InternetAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     InternetAddr.sin_port = htons(PORT);
 
+	// ---------------------------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------------------------
+
+	//
+	// 바인드
     if (bind(ListenSocket, (PSOCKADDR)&InternetAddr, sizeof(InternetAddr)) == SOCKET_ERROR)
     {
         printf("bind() failed with error %d\n", WSAGetLastError());
@@ -83,7 +96,14 @@ int main(int argc, char **argv)
     }
     else
         printf("listen() is OK maa...\n");
+	
+	
+	// ---------------------------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------------------------
 
+
+	//
+	// 소켓 만듬
     // Setup the listening socket for connections
     if ((AcceptSocket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED)) == INVALID_SOCKET)
     {
@@ -93,6 +113,13 @@ int main(int argc, char **argv)
     else
         printf("WSASocket() looks OK!\n");
 
+
+	// ---------------------------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------------------------
+
+
+	//
+	// 이벤트 세팅
     if ((EventArray[0] = WSACreateEvent()) == WSA_INVALID_EVENT)
     {
         printf("WSACreateEvent() failed with error %d\n", WSAGetLastError());
@@ -101,6 +128,12 @@ int main(int argc, char **argv)
     else
         printf("WSACreateEvent() is OK!\n");
 
+	// ---------------------------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------------------------
+
+
+	//
+	// 스레드 생성
     // Create a thread to service overlapped requests
     if (CreateThread(NULL, 0, ProcessIO, NULL, 0, &ThreadId) == NULL)
     {
@@ -110,10 +143,13 @@ int main(int argc, char **argv)
     else
         printf("Nothing to say, CreateThread() is OK!\n");
 
-    EventTotal = 1;
+    EventTotal = 1;//
 
     while (TRUE)
     {
+		//
+		// accept
+		// 접속 발생시 소켓 리턴
         // Accept inbound connections
         if ((AcceptSocket = accept(ListenSocket, NULL, NULL)) == INVALID_SOCKET)
         {
@@ -124,6 +160,8 @@ int main(int argc, char **argv)
             printf("accept() is OK!\n");
 
         EnterCriticalSection(&CriticalSection);
+
+		// 소켓이 하나씩 만들어짐
         // Create a socket information structure to associate with the accepted socket
         if ((SocketArray[EventTotal] = (LPSOCKET_INFORMATION)GlobalAlloc(GPTR, sizeof(SOCKET_INFORMATION))) == NULL)
         {
@@ -141,6 +179,7 @@ int main(int argc, char **argv)
         SocketArray[EventTotal]->DataBuf.len = DATA_BUFSIZE;
         SocketArray[EventTotal]->DataBuf.buf = SocketArray[EventTotal]->Buffer;
 
+		// Overlapped와 EventArray에 이벤트 복사
         if ((SocketArray[EventTotal]->Overlapped.hEvent = EventArray[EventTotal] = WSACreateEvent()) == WSA_INVALID_EVENT)
         {
             printf("WSACreateEvent() failed with error %d\n", WSAGetLastError());
@@ -151,6 +190,14 @@ int main(int argc, char **argv)
 
         // Post a WSARecv() request to to begin receiving data on the socket
         Flags = 0;
+
+		// ---------------------------------------------------------------------------------------------------------
+		// ---------------------------------------------------------------------------------------------------------
+
+
+		// 중요
+		// 리시브 호출 필수
+		// 한번 호출해야 통지받을 수 있음
         if (WSARecv(SocketArray[EventTotal]->Socket,
             &(SocketArray[EventTotal]->DataBuf), 1, &RecvBytes, &Flags, &(SocketArray[EventTotal]->Overlapped), NULL) == SOCKET_ERROR)
         {
@@ -190,6 +237,14 @@ DWORD WINAPI ProcessIO(LPVOID lpParameter)
     // Process asynchronous WSASend, WSARecv requests
     while (TRUE)
     {
+
+		// ---------------------------------------------------------------------------------------------------------
+		// ---------------------------------------------------------------------------------------------------------
+
+
+		//
+		// EventArray 0번에는 accept
+		// 이후에는 클라이언트 들어있음
         if ((Index = WSAWaitForMultipleEvents(EventTotal, EventArray, FALSE, WSA_INFINITE, FALSE)) == WSA_WAIT_FAILED)
         {
             printf("WSAWaitForMultipleEvents() failed %d\n", WSAGetLastError());
@@ -198,6 +253,14 @@ DWORD WINAPI ProcessIO(LPVOID lpParameter)
         else
             printf("WSAWaitForMultipleEvents() is OK!\n");
 
+
+		// ---------------------------------------------------------------------------------------------------------
+		// ---------------------------------------------------------------------------------------------------------
+
+
+		//
+		// accept는 통지받을 수 있도록 리턴시켜줌
+		// 0번째가 아닐경우 read IO 이벤트 (패킷 받음)
         // If the event triggered was zero then a connection attempt was made
         // on our listening socket.
         if ((Index - WSA_WAIT_EVENT_0) == 0)
@@ -206,13 +269,25 @@ DWORD WINAPI ProcessIO(LPVOID lpParameter)
             continue;
         }
 
+
+		// ---------------------------------------------------------------------------------------------------------
+		// ---------------------------------------------------------------------------------------------------------
+
+
+		//
+		// 리시브로 이벤트 통지를 받기 위해 리셋
         SI = SocketArray[Index - WSA_WAIT_EVENT_0];
         WSAResetEvent(EventArray[Index - WSA_WAIT_EVENT_0]);
 
+		//
+		// 결과값 얻음
+		// BytesTransferred에 리턴
         if (WSAGetOverlappedResult(SI->Socket, &(SI->Overlapped), &BytesTransferred, FALSE, &Flags) == FALSE || BytesTransferred == 0)
         {
             printf("Closing socket %d\n", SI->Socket);
 
+			//
+			// 에러 시 클로즈
             if (closesocket(SI->Socket) == SOCKET_ERROR)
             {
                 printf("closesocket() failed with error %d\n", WSAGetLastError());
@@ -240,6 +315,8 @@ DWORD WINAPI ProcessIO(LPVOID lpParameter)
         // Check to see if the BytesRECV field equals zero. If this is so, then
         // this means a WSARecv call just completed so update the BytesRECV field
         // with the BytesTransferred value from the completed WSARecv() call.
+
+		// 다받으면 send 상태로 만듬
         if (SI->BytesRECV == 0)
         {
             SI->BytesRECV = BytesTransferred;
@@ -252,6 +329,7 @@ DWORD WINAPI ProcessIO(LPVOID lpParameter)
 
         if (SI->BytesSEND < SI->BytesRECV )
         {
+			//
             // Post another WSASend() request.
             // Since WSASend() is not guaranteed to send all of the bytes requested,
             // continue posting WSASend() calls until all received bytes are sent
@@ -261,6 +339,8 @@ DWORD WINAPI ProcessIO(LPVOID lpParameter)
             SI->DataBuf.buf = SI->Buffer + SI->BytesSEND;
             SI->DataBuf.len = SI->BytesRECV - SI->BytesSEND;
 
+			//
+			// send가 끝났다고해서 가만있으면 리시브 할 수 없음
             if (WSASend(SI->Socket, &(SI->DataBuf), 1, &SendBytes, 0, &(SI->Overlapped), NULL) == SOCKET_ERROR)
             {
                 if (WSAGetLastError() != ERROR_IO_PENDING)
@@ -272,7 +352,8 @@ DWORD WINAPI ProcessIO(LPVOID lpParameter)
             else
                 printf("WSASend() is OK!\n");
         }
-        else
+		// 리시브 다함
+        else 
         {
             SI->BytesRECV = 0;
             // Now that there are no more bytes to send post another WSARecv() request
@@ -283,6 +364,8 @@ DWORD WINAPI ProcessIO(LPVOID lpParameter)
             SI->DataBuf.len = DATA_BUFSIZE;
             SI->DataBuf.buf = SI->Buffer;
 
+			//
+			// 리시브를 호출해서 통지받을 수 있도록 해야함
             if (WSARecv(SI->Socket, &(SI->DataBuf), 1, &RecvBytes, &Flags, &(SI->Overlapped), NULL) == SOCKET_ERROR)
             {
                 if (WSAGetLastError() != ERROR_IO_PENDING)
